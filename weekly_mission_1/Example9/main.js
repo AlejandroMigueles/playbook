ec0d7982708bb730eacd66da4346c6c3e5e7e56a
Alejandro Migueles <alexmigueles@gmail.com>
2022-04-08 22:05:11 -0500

    se agrega evolucion de pokemon

--- weekly_mission_1/Example9/main.js
+++ weekly_mission_1/Example9/main.js
@@ -3,7 +3,7 @@ const Pokemon = require('./pokemon')
 const pikachu = new Pokemon("pikachu")
 const bulbasaur = new Pokemon("bulbasaur")
 const squirtle = new Pokemon("squirtle")
-const charmander = new Pokemon("charmander")
+const charmander = new Pokemon("charizard")
 
 pikachu.sayHello()
 pikachu.sayMessage("Heey!")
